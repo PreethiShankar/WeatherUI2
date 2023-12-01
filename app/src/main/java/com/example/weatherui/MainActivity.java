031314dfd5a1b8c48cde7ee4fba18f78c449caac
Preethi <pshanka8@asu.edu>
2023-11-30 17:23:08 -0700

    UI Updates

--- app/src/main/java/com/example/weatherui/MainActivity.java
+++ app/src/main/java/com/example/weatherui/MainActivity.java
@@ -12,9 +12,9 @@ import androidx.cardview.widget.CardView;
 public class MainActivity extends AppCompatActivity {
 
     //Setting values for temperature, precipitation and humidity
-    private int temperature = 29;
-    private int precipitationPercentage = 90;
-    private int humidity = 57;
+    private int temperature = 104;
+    private int precipitationPercentage = 30;
+    private int humidity = 58;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setMax(100);
         progressBar.setProgress(temperature);
         progressBar.setTemperature(temperature);
-
-
 
         TextView precipitationTextView = findViewById(R.id.actualRainTextView);
         TextView humidityTextView = findViewById(R.id.actualhumidity);
@@ -52,27 +50,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        CardView firstCardView = findViewById(R.id.firstCardView);
-
+        CardView firstCardView = findViewById(R.id.idfirstCardView);
 
         firstCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Create an Intent to navigate to the Calendar activity
                 Intent intent = new Intent(MainActivity.this, Calendar.class);
                 startActivity(intent);
             }
         });
+
+
+        CardView secondCardView = findViewById(R.id.idsecondCardView);
+
+
+        secondCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the Calendar activity
+                Intent intent = new Intent(MainActivity.this, Exploreaqi.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
     private void setDynamicBackground(HalfCircularProgressBar progressBar) {
         RelativeLayout mainLayout = findViewById(R.id.RelativeLayoutId1);
 
-        if (temperature < 5) {
-            mainLayout.setBackgroundResource(R.drawable.bluesnow);
-        } else if (precipitationPercentage > 80) {
-            mainLayout.setBackgroundResource(R.drawable.rainyfinal);
-        } else {
+        if (precipitationPercentage > 80) {
+            mainLayout.setBackgroundResource(R.drawable.rainyfinal);}
+        else if (temperature < 5) {
+                mainLayout.setBackgroundResource(R.drawable.bluesnow);}
+         else {
             mainLayout.setBackgroundResource(R.drawable.sunnyfinal);
         }
     }
